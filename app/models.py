0df7b9c46e6ac5ba18275c2cec519d68d69eaad8
SAMBIT CHAKRABORTY <smbtchakraborty@gmail.com>
2023-06-27 12:26:23 +0530

    Updated

--- app/models.py
+++ app/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
+'''
+the below class is the model for the tag and it has the following fields:
+name: the name of the tag
+description: the description of the tag
+slug: the slug of the tag
+'''
+class Tag (models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def save (self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+    
 '''
 the below class is the model for the blog post and it has the following fields:
 title: the title of the post
@@ -15,6 +35,7 @@ class Post (models.Model):
 
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(null = True, blank=True, upload_to="images/")
+    tags = models.ManyToManyField(Tag , blank=True, related_name='post')
 
 
     def __str__(self):
