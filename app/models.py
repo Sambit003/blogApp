d7d1a25f31e0fae019b8e534689d2ae77f1d6f49
SAMBIT CHAKRABORTY <smbtchakraborty@gmail.com>
2023-07-05 22:54:31 +0530

    Feat: View count, comment, replies

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
 
@@ -7,19 +8,22 @@ name: the name of the tag
 description: the description of the tag
 slug: the slug of the tag
 '''
-class Tag (models.Model):
+
+
+class Tag(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
 
-    def save (self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.name)
         return super(Tag, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.name
-    
+
+
 '''
 the below class is the model for the blog post and it has the following fields:
 title: the title of the post
@@ -28,15 +32,38 @@ last_modified: the date and time the post was last modified
 slug: the slug of the post
 image: the image of the post
 '''
-class Post (models.Model):
+
+
+class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     last_modified = models.DateTimeField(auto_now=True)
 
     slug = models.SlugField(max_length=200, unique=True)
-    image = models.ImageField(null = True, blank=True, upload_to="images/")
-    tags = models.ManyToManyField(Tag , blank=True, related_name='post')
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+    tags = models.ManyToManyField(Tag, blank=True, related_name='post')
+    view_count = models.IntegerField(null=True, blank=True)
 
 
-    def __str__(self):
-        return self.title
+'''
+the below class is the model for the comments and it has the following fields:
+content: the content of the comment
+date: the date and time the comment was posted
+name: the name of the person who posted the comment
+email: the email of the person who posted the comment
+website: the website of the person who posted the comment
+post: the post the comment was posted on
+author: the author of the comment
+parent: the parent comment of the comment (if it is a reply)
+'''
+
+
+class Comments(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    website = models.CharField(max_length=200, null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank= True, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True, related_name='replies')
--- app/views.py
+++ app/views.py
@@ -1,13 +1,50 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from app.models import Post
+from django.urls import reverse
+
+from app.forms import CommentForm
+from app.models import Post, Comments
+
 '''
 the below function is the view for the home page and it returns the home.html template
 '''
+
+
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
-    context = {'post': post}
+    comments = Comments.objects.filter(post=post, parent=None)
+    form = CommentForm()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            parent_obj = None
+            if request.POST.get('parent'):
+                parent = request.POST.get('parent')
+                parent_obj = Comments.objects.get(id=parent)
+                if parent_obj:
+                    comment_reply = comment_form.save(commit=False)
+                    comment_reply.parent = parent_obj
+                    comment_reply.post = post
+                    comment_reply.save()
+                    return HttpResponseRedirect(reverse('post_page', kwargs={'slug': slug}))
+            else:
+                comment = comment_form.save(commit=False) # commit=False means that the comment is not saved to the database
+                post_id = request.POST.get('post_id') # get the post id from the hidden input in the form
+                post = Post.objects.get(id = post_id)
+                comment.post = post # set the post of the comment to the post we got from the database
+                comment.save()
+                return HttpResponseRedirect(reverse('post_page', kwargs={'slug': slug}))
+
+    if post.view_count is None:
+        post.view_count = 1
+    else:
+        post.view_count += 1
+    post.save()
+    context = {'post': post, 'form': form, 'comments': comments}
     return render(request, 'app/post.html', context)
+
 
 def index(request):
     context = {'posts': Post.objects.all()}
-    return render(request, 'app/index.html', )
+    return render(request, 'app/index.html', context)
